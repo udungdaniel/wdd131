5e3920d09785b10d485974036681be4a07945261
udungdaniel <danielalfred783@gmail.com>
2025-05-10 23:11:09 +0100

    wk01 assignment update

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// get current year
+const yearSpan = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+// get last modified date
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
